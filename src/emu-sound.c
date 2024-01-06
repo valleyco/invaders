5962192ca7329e71031c4925c2402cdf5e3ee1fc
David Levy <levydav@gmail.com>
2024-01-06 18:11:21 +0200

    shorter sound callback buffer

--- src/emu-sound.c
+++ src/emu-sound.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include "emu-sound.h"
 typedef enum
@@ -12,6 +13,22 @@ typedef enum
     UFO_HIGH_PITCH,
     UFO_LOW_PITCH,
 } Sounds;
+
+typedef enum
+{
+    BIT_UFO_LOW_PITCH = 1,
+    BIT_SHOOT = 2,
+    BIT_INVADER_KILLED = 8,
+    BIT_UFO_HIGH_PITCH,
+} SoundBits_0;
+enum
+{
+    BIT_FAST_INVADER_1 = 1,
+    BIT_FAST_INVADER_2 = 2,
+    BIT_FAST_INVADER_3 = 4,
+    BIT_FAST_INVADER_4 = 8,
+    BIT_EXPLOSION = 16
+} SoundBits_2;
 
 static const char *wave_file_name[] = {
     "../wav/shoot.wav",
@@ -37,6 +54,7 @@ typedef struct
 {
     int wave_id;
     Uint32 audio_pos; // position to the audio buffer to be played
+    bool is_playing;  // position to the audio buffer to be played
 
 } CallbackData;
 
@@ -56,8 +74,9 @@ static void load_wave_files()
         audio16_buffer[i] = malloc(sizeof(Sint16) * audio_buffer_len[i]);
         for (Uint32 s = 0; s < audio_buffer_len[i]; s++)
         {
-            audio16_buffer[i][s] = ((Sint8)audio_buffer[s]) * 64;
+            audio16_buffer[i][s] = ((Uint8)audio_buffer[s]) * 64;
         }
+        callback_data[i].audio_pos = audio_buffer_len[i];
         SDL_FreeWAV(audio_buffer);
     }
 }
@@ -72,40 +91,53 @@ static void port_write(SoundDevice *g, int p, int v)
     switch (p)
     {
     case 0:
-        if (v & 1)
+        callback_data[UFO_LOW_PITCH].is_playing = v & BIT_UFO_LOW_PITCH;
+        callback_data[SHOOT].is_playing = v & BIT_SHOOT;
+        callback_data[INVADER_KILLED].is_playing = v & BIT_INVADER_KILLED;
+
+        if (!callback_data[UFO_LOW_PITCH].is_playing)
         {
             callback_data[UFO_LOW_PITCH].audio_pos = 0;
         }
-        if (v & 2)
+        if (!callback_data[SHOOT].is_playing)
         {
             callback_data[SHOOT].audio_pos = 0;
         }
-        if (v & 8)
+        if (!callback_data[INVADER_KILLED].is_playing)
         {
             callback_data[INVADER_KILLED].audio_pos = 0;
         }
         break;
     case 2:
-        if (v & 1)
+        callback_data[FAST_INVADER_1].is_playing = v & BIT_FAST_INVADER_1;
+        callback_data[FAST_INVADER_2].is_playing = v & BIT_FAST_INVADER_2;
+        callback_data[FAST_INVADER_3].is_playing = v & BIT_FAST_INVADER_3;
+        callback_data[FAST_INVADER_4].is_playing = v & BIT_FAST_INVADER_4; 
+        callback_data[EXPLOSION].is_playing = v & BIT_EXPLOSION;
+        if (!callback_data[FAST_INVADER_1].is_playing)
         {
             callback_data[FAST_INVADER_1].audio_pos = 0;
         }
-        if (v & 2)
+        if (!callback_data[FAST_INVADER_2].is_playing)
         {
             callback_data[FAST_INVADER_2].audio_pos = 0;
         }
-        if (v & 4)
+        if (!callback_data[FAST_INVADER_3].is_playing)
         {
             callback_data[FAST_INVADER_3].audio_pos = 0;
         }
-        if (v & 8)
+        if (!callback_data[FAST_INVADER_4].is_playing)
         {
             callback_data[FAST_INVADER_4].audio_pos = 0;
         }
+        if (!callback_data[EXPLOSION].is_playing)
+        {
+            callback_data[EXPLOSION].audio_pos = 0;
+        }
+
         break;
     }
 }
-
 
 static int (*port_read_array[])(SoundDevice *g, int p) = {NULL, NULL, NULL};
 
@@ -117,12 +149,12 @@ SoundDevice *emu_sound_init()
     {
         if (SDL_Init(SDL_INIT_AUDIO) == 0)
         {
-            load_wave_files();
             SDL_memset(callback_data, 0, sizeof(callback_data));
+            load_wave_files();
             dev_wav_spec.channels = 1;
             dev_wav_spec.freq = 11025;
             dev_wav_spec.silence = 128;
-            dev_wav_spec.samples = 2048;
+            dev_wav_spec.samples = 512;
             dev_wav_spec.padding = 0;
             dev_wav_spec.size = 0;
             dev_wav_spec.format = 0x8010;
@@ -162,19 +194,21 @@ void emu_sound_done(SoundDevice *dev)
 
 static void audio_callback(CallbackData *userdata, Uint8 *stream, int len)
 {
+    // printf("---------\n");
     SDL_memset(stream, 0, len);
     for (int i = 0; i < SOUND_COUNT; i++)
     {
         const int total_len = audio_buffer_len[i];
         const Uint32 audio_pos = userdata[i].audio_pos;
-        const int left = total_len - audio_pos;
-        if (left == 0)
+        const int bytes_left = (total_len - audio_pos) * (int)sizeof(Sint16);
+        if (!userdata[i].is_playing || bytes_left == 0)
         {
             continue;
         }
-        int count = len > left ? left : len;
-        // printf("sound: %d,len:%d, left: %d, count: %d\n", i,len, left, count);
-        SDL_MixAudio(stream, (Uint8 *)(audio16_buffer[i] + audio_pos), count, SDL_MIX_MAXVOLUME / 4); // mix from one buffer into another
-        userdata[i].audio_pos += count;
+        int byte_count = len > bytes_left ? bytes_left : len;
+        // printf("\tb: sound: %d,total_len:%d, audio_pos: %d, byte_count: %d\n", i, total_len, userdata[i].audio_pos, byte_count);
+        SDL_MixAudioFormat(stream, (Uint8 *)(audio16_buffer[i] + audio_pos), dev_wav_spec.format, byte_count, SDL_MIX_MAXVOLUME); // mix from one buffer into another
+        userdata[i].audio_pos += byte_count / sizeof(Sint16);
+        // printf("\ta: sound: %d,total_len:%d, audio_pos: %d, byte_count: %d\n", i,total_len, userdata[i].audio_pos, byte_count);
     }
 }
